d92f17cd60933ea5f145335c67445408a2193b27
Alfonso <90729891+AlfonsoEstudiante@users.noreply.github.com>
2025-10-27 16:41:14 -0600

    Add files via upload

---
+++ PrimerParcial_AlfonsoMadero/PrimerParcial_AlfonsoMadero/PrimerParcial_AlfonsoMaderoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimerParcial_AlfonsoMaderoApp.swift
+//  PrimerParcial_AlfonsoMadero
+//
+//  Created by win603 on 15/09/25.
+//
+
+import SwiftUI
+
+@main
+struct PrimerParcial_AlfonsoMaderoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
